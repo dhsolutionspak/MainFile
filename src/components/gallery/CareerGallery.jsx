4aea79fb301be6822f1b0e128398948587d6b069
dhsolutionspak <digitalhousepakistan@gmail.com>
2024-10-31 19:23:03 +0500

    photo index

--- src/components/gallery/CareerGallery.jsx
+++ src/components/gallery/CareerGallery.jsx
@@ -1,5 +1,5 @@
-import Career2 from "../../../public/assets/imgs/career/2.jpg";
-import Career3 from "../../../public/assets/imgs/career/3.jpeg";
+import Career2 from "../../../public/assets/imgs/career/2.jpeg";
+import Career3 from "../../../public/assets/imgs/career/3.jpg";
 import Career4 from "../../../public/assets/imgs/career/4.jpg";
 import Career5 from "../../../public/assets/imgs/career/5.jpg";
 import Image from "next/image";
