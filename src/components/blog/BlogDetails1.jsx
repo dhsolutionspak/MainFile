70c0cdd044cad1e59b4d0afb9b86e8cf76aba13d
dhsolutionspak <digitalhousepakistan@gmail.com>
2024-11-02 18:33:51 +0500

    Update BlogDetails1.jsx

--- src/components/blog/BlogDetails1.jsx
+++ src/components/blog/BlogDetails1.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Author from "../../../public/assets/imgs/blog/detail/author.png";
-import Detail1 from "../../../public/assets/imgs/blog/detail/1.jpg";
-import Detail2 from "../../../public/assets/imgs/blog/detail/2.jpg";
-import Detail3 from "../../../public/assets/imgs/blog/detail/3.jpg";
+import Detail1 from "../../../public/assets/imgs/blog/detail/1.jpg"; // Update this with a relevant image for cybersecurity
+import Detail2 from "../../../public/assets/imgs/blog/detail/2.jpg"; // Update this with a relevant image for cybersecurity
+import Detail3 from "../../../public/assets/imgs/blog/detail/3.jpg"; // Update this with a relevant image for cybersecurity
 import { useEffect, useRef } from "react";
 import animationWordCome from "@/lib/utils/animationWordCome";
 
@@ -27,13 +27,13 @@ const BlogDetails1 = () => {
                   className="blog__detail-date animation__word_come"
                   ref={wordAnim}
                 >
-                  Design, Marketing <span>25 Jan 2019</span>
+                  Cybersecurity <span>{new Date().toLocaleDateString()}</span>
                 </h2>
                 <h3
                   className="blog__detail-title animation__word_come"
                   ref={wordAnim2}
                 >
-                  Donate your design for newest designers to try better
+                  The Importance of Cybersecurity in Today’s Digital Landscape
                 </h3>
                 <div className="blog__detail-metalist">
                   <div className="blog__detail-meta">
@@ -44,12 +44,12 @@ const BlogDetails1 = () => {
                       alt="Author Picture"
                     />
                     <p>
-                      Writen by <span>Codexpand</span>
+                      Written by <span>Codexpand</span>
                     </p>
                   </div>
                   <div className="blog__detail-meta">
                     <p>
-                      Viewed <span>3 min read</span>
+                      Viewed <span>5 min read</span>
                     </p>
                   </div>
                 </div>
@@ -60,7 +60,7 @@ const BlogDetails1 = () => {
                 <Image
                   priority
                   style={{ width: "auto", height: "auto" }}
-                  src={Detail1}
+                  src={Detail1} // Use an image relevant to the blog topic
                   alt="Blog Thumbnail"
                   data-speed="0.5"
                 />
@@ -69,76 +69,58 @@ const BlogDetails1 = () => {
             <div className="col-xxl-8 col-xl-10 offset-xxl-2 offset-xl-1">
               <div className="blog__detail-content">
                 <p>
-                  We love to bring designs to life as a developer, and I aim to
-                  do this using whatever front end tools are necessary. My
-                  preferred tools are more modern javascript libraries like
-                  React.js but I like to use whatever is best for the websites
-                  needs. There are several reasons why a business would consider
-                  a rebrand and it doesn’t necessarily mean the business has
-                  been unsuccessful.{" "}
+                  In our increasingly connected world, cybersecurity has become a
+                  critical concern for individuals and organizations alike. With
+                  the rapid advancement of technology and the rising sophistication
+                  of cyber threats, understanding and implementing effective
+                  cybersecurity measures is essential.
                 </p>
                 <p>
-                  But in order that you may see whence all this born error of
-                  those who accuse pleasure and praise pain, I will open the
-                  whole matter, and explain the very things which were said by
-                  that discoverer of truth and, as it were, the architect of a
-                  happy life.
+                  Cybersecurity safeguards sensitive data such as personal
+                  information, financial records, and intellectual property. A
+                  breach can lead to identity theft, financial loss, and damage to
+                  an organization’s reputation.
                 </p>
                 <Image
                   priority
                   width={850}
                   style={{ height: "auto" }}
-                  src={Detail2}
+                  src={Detail2} // Use an image relevant to the blog topic
                   alt="Blog Image"
                 />
-                <h2>JavaScript</h2>
+                <h2>Common Cyber Threats</h2>
                 <p>
-                  We love to bring designs to life as a developer, and I aim to
-                  do this using whatever front end tools are necessary. My
-                  preferred tools are more modern javascript libraries like
-                  React.js but I like to use whatever is best for the websites
-                  needs. There are several reasons why a business would consider
-                  a rebrand and it doesn’t necessarily mean the business has
-                  been unsuccessful.
+                  Cyberattacks can have devastating financial implications. The cost
+                  of recovering from a data breach can be astronomical, not to
+                  mention the potential legal liabilities and fines that may arise
+                  from non-compliance with regulations like GDPR and HIPAA.
                 </p>
-                <h2>Fremework</h2>
+                <h2>Best Practices for Cybersecurity</h2>
                 <p>
-                  Always ready to push the boundaries, especially when it comes
-                  to our own platform, Our analytical eye to create a site that
-                  was visually engaging and also optimised for maximum
-                  performance. It also perfectly reflects the journey to help it
-                  tell a story to increase its understanding and drive action.
-                  To create a site that was visually engaging for maximum
-                  performance.
+                  Regular training sessions on recognizing phishing attempts and
+                  safe browsing practices can significantly reduce the risk of
+                  successful attacks. An informed workforce is a strong defense
+                  against cyber threats.
                 </p>
                 <ul>
-                  <li>Brand Development</li>
-                  <li>UX/UI Design</li>
-                  <li>Front-end Development</li>
-                  <li>Copywriting</li>
-                  <li>Shopify Development</li>
+                  <li>Educate Employees</li>
+                  <li>Implement Strong Password Policies</li>
+                  <li>Regularly Update Software</li>
+                  <li>Conduct Security Audits</li>
+                  <li>Back Up Data</li>
                 </ul>
-                <h2>Visual Studio</h2>
                 <p>
-                  Just like other pseudo-elements and pseudo-class selectors,
-                  :not() can be chained with other pseudo-classes and
-                  pseudo-elements. For example, the following will add a “New!”
-                  word to list items that do not have a .old class name, using
-                  the ::after pseudo-element:
+                  As cyber threats continue to evolve, the importance of robust
+                  cybersecurity measures cannot be overstated. By prioritizing
+                  cybersecurity, individuals and organizations can protect their
+                  sensitive data and maintain business continuity.
                 </p>
-                <Image
-                  priority
-                  width={850}
-                  style={{ height: "auto" }}
-                  src={Detail3}
-                  alt="Code"
-                />
               </div>
               <div className="blog__detail-tags">
                 <p className="sub-title-anim">
-                  tags: <Link href="/blog">design</Link>,{" "}
-                  <Link href="/blog">figma</Link>,
-                  <Link href="/tag">update</Link>
+                  tags: <Link href="/blog">cybersecurity</Link>,{" "}
+                  <Link href="/blog">data protection</Link>,{" "}
+                  <Link href="/tag">best practices</Link>
                 </p>
               </div>
             </div>
